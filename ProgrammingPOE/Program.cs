88950b0d5a6c61cd545fc068ca7de929e4c9b386
Bradley5647 <bappenah@gmail.com>
2025-05-21 23:18:33 +0200

    Add project files.

---
+++ ProgrammingPOE/Program.cs
@@ -0,0 +1,27 @@
+﻿using ProgPOE;
+
+namespace ProgrammingPOE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            visual v = new visual();
+            UserResponseSystem response = new UserResponseSystem();
+
+            //main calling the methods
+            v.logo();
+            v.Audio();
+            response.ResponseSystem();
+
+            Console.WriteLine("Thank you for using the program");
+
+            Console.ReadLine();
+        }
+
+        //references
+        //stackflow
+        // youtbe video Nelson, 2022 URLhttps://www.bing.com/videos/riverview/relatedvideo?q=youtube+video+explaining+how+to+add+audio
+    }
+}
+
---
+++ ProgrammingPOE/visual.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingPOE
+{
+    class visual
+    {
+            //class for the audio. uses play sync to play promt user after the recording
+            public void Audio()
+            {
+                SoundPlayer play = new SoundPlayer("./aud.wav");
+                play.PlaySync();
+
+
+            }
+            //designs method to display the logo
+            public void logo()
+            {
+                String logo = @"                                                     __
+                              .__                         / |
+                             /  /                         |  \
+                            /   |                     ,-------'_
+                       ____/     \_________,     ,_--""      _/  \_
+         _______------'                     `---""          ,-\___/
+     _--""                                               ,-""
+ ___(___                                          ,__--""
+(-------0       cybersecurity awareness     __---""
+ `--___                                    /
+       `--___\                _______-----'
+             \\    (____-----'
+              \\    \_
+               `""`..__\";
+
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(logo);
+                Console.WriteLine("--------------------------------------------------------");
+                Console.ResetColor();
+
+            }
+
+        }
+    }
+
+
